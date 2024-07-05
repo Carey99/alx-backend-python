8f5ceb3a9146edfaa28125e9a89ba5142bf18f75
Carey99 <edwinakidah1@gmail.com>
2024-07-05 03:08:08 +0300

    More involved type annotations

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -2,12 +2,16 @@
 """More involved type annotations"""
 
 
-from typing import Tuple, List
+from typing import Any, TypeVar, Union, Mapping, Tuple, Dict
 
 
-def safely_get_value(dct: dict, key: str, default: int = 0) -> Tuple:
+T = TypeVar('T')
+ResponseType = Union[T, None]
+Def1 = Union[T, None]
+
+def safely_get_value(dct: Mapping, key: Any, default: Def1 = 0) -> ResponseType:
     """Return a tuple containing the value and the key"""
     if key in dct:
-        return (dct[key], key)
+        return (dct[key])
     else:
-        return (default, key)
+        return (default)
