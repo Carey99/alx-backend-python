bddf38d02e446f1b022a8e00cc3f14266b5257c4
Carey99 <edwinakidah1@gmail.com>
2024-07-09 03:37:56 +0300

    Task

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
-"""create a task_wait_random function with int max_delay"""
+""""Use regular func and write task_wait_random coroutine
+    that takes an int max_delay and returns a asyncio.Task
+"""
 import asyncio
-from typing import Awaitable
+import random
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> asyncio.Task[Awaitable[str]]:
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """returns a asyncio.Task"""
     return asyncio.create_task(wait_random(max_delay))
